9d51f82eb6b4e60267aeeeba4e857848ab3b166b
digital <digital@qq.com>
2020-10-23 11:43:51 +0800

    a

---
+++ web/src/layout/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="layout">
+      layout
+      <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- web/src/main.js
+++ web/src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+import ElementUI from 'element-ui'
+import store from './store'
+import router from './router'
+import '@/permission'
 
 Vue.config.productionTip = false
 
+Vue.use(ElementUI)
+
 new Vue({
   render: h => h(App),
+  router,
+  store
 }).$mount('#app')
